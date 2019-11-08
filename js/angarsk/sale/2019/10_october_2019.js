11e69e5e2554def6b96910c1419e516e37f2a092
IrkStat <51128435+irkstat@users.noreply.github.com>
2019-11-09 00:35:11 +0800

    0.0.11.0
    -Создана страница Ангарск продажа за октябрь 2019
    Тестирование:
    -Скорость: моб(93), комп(99)
    -Проверка на валибность: ошибок нет

---
+++ js/angarsk/sale/2019/10_october_2019.js
@@ -0,0 +1,433 @@
+Chart.defaults.global.defaultFontColor = 'black'; //глобально цвет шрифта
+Chart.defaults.global.defaultFontFamily = "Lato"; // тип шрифта
+Chart.defaults.global.defaultFontSize = 16; // глобально размер шрифта
+
+// Первичный рынок жилья Иркутск
+var ctx = document.getElementById('apartment');
+var apartment = new Chart(ctx, {
+   type: 'bar',
+   data: {
+      labels: ['11.18', '12.18', '01.19', '02.19', '03.19', '04.19', '05.19', '06.19', '07.19', '08.19', '09.19', '10.19'],
+      datasets: [{
+         label: 'Стоимость кв.м.',
+         data: [47780, 42450, 48860, 48630, 45400, 44880, 46230, 51020, 45600, 46420, 46260, 44080],
+         backgroundColor: 'gray',
+         borderColor: '#293133',
+         borderWidth: 1,
+      }]
+   },
+   options: {
+      legend: {
+         display: true,
+         position: 'bottom'
+      },
+      scales: {
+
+         yAxes: [{
+            ticks: {
+               beginAtZero: false,
+               min: 35000,
+               stepSize: 5000,
+            },
+            gridLines: {
+               color: "black",
+               borderDash: [2, 5],
+            },
+         }],
+
+         xAxes: [{
+            gridLines: {
+               display: false,
+            },
+         }]
+      }
+   }
+});
+
+
+// Вторичный рынок жилья Иркутск
+var ctx = document.getElementById('apartment2');
+var apartment2 = new Chart(ctx, {
+   type: 'bar',
+   data: {
+      labels: ['11.18', '12.18', '01.19', '02.19', '03.19', '04.19', '05.19', '06.19', '07.19', '08.19', '09.19', '10.19'],
+      datasets: [{
+         label: 'Стоимость кв.м.',
+         data: [40740, 40740, 41300, 42260, 41850, 41880, 41710, 41820, 41880, 41870, 42210, 42470],
+         backgroundColor: '#ab7d5c',
+         borderColor: '#293133',
+         borderWidth: 1,
+      }]
+   },
+   options: {
+      //Управление легендой диаграммы
+      legend: {
+         display: true,
+         position: 'bottom'
+      },
+      scales: {
+
+         yAxes: [{
+            ticks: {
+               beginAtZero: false,
+               min: 38000,
+               stepSize: 2000,
+            },
+            gridLines: {
+               color: "black",
+               borderDash: [2, 5],
+            },
+         }],
+
+         xAxes: [{
+            gridLines: {
+               display: false,
+            },
+         }]
+      }
+   }
+});
+
+// Гаражи железобетонный Иркутск
+var ctx = document.getElementById('garage');
+var garage = new Chart(ctx, {
+   type: 'bar',
+   data: {
+      labels: ['11.18', '12.18', '01.19', '02.19', '03.19', '04.19', '05.19', '06.19', '07.19', '08.19', '09.19', '10.19'],
+      datasets: [{
+         label: 'Стоимость кв.м.',
+         data: [12060, 11990, 11610, 12020, 11830, 13250, 14490, 14500, 14880, 14030, 13490, 13650],
+         backgroundColor: 'gray',
+         borderColor: '#293133',
+         borderWidth: 1,
+      }]
+   },
+   options: {
+      //Управление легендой диаграммы
+      legend: {
+         display: true,
+         position: 'bottom'
+      },
+      scales: {
+
+         yAxes: [{
+            ticks: {
+               beginAtZero: false,
+               min: 10000,
+               stepSize: 2000,
+            },
+            gridLines: {
+               color: "black",
+               borderDash: [2, 5],
+            },
+         }],
+
+         xAxes: [{
+            gridLines: {
+               display: false,
+            },
+         }]
+      }
+   }
+});
+
+// Гаражи кирпич Иркутск
+var ctx = document.getElementById('garage2');
+var garage2 = new Chart(ctx, {
+   type: 'bar',
+   data: {
+      labels: ['11.18', '12.18', '01.19', '02.19', '03.19', '04.19', '05.19', '06.19', '07.19', '08.19', '09.19', '10.19'],
+      datasets: [{
+         label: 'Стоимость кв.м.',
+         data: [14170, 12300, 13750, 13390, 13910, 14680, 15330, 14920, 14990, 14210, 15230, 15020],
+         backgroundColor: '#ab7d5c',
+         borderColor: '#293133',
+         borderWidth: 1,
+      }]
+   },
+   options: {
+      //Управление легендой диаграммы
+      legend: {
+         display: true,
+         position: 'bottom'
+      },
+      scales: {
+
+         yAxes: [{
+            ticks: {
+               beginAtZero: false,
+               min: 10000,
+               stepSize: 2000,
+            },
+            gridLines: {
+               color: "black",
+               borderDash: [2, 5],
+            },
+         }],
+
+         xAxes: [{
+            gridLines: {
+               display: false,
+            },
+         }]
+      }
+   }
+});
+
+// Земли поселений Иркутск
+var ctx = document.getElementById('earth');
+var earth = new Chart(ctx, {
+   type: 'bar',
+   data: {
+      labels: ['11.18', '12.18', '01.19', '02.19', '03.19', '04.19', '05.19', '06.19', '07.19', '08.19', '09.19', '10.19'],
+      datasets: [{
+         label: 'Стоимость кв.м.',
+         data: [38220, 46590, 41310, 47910, 40080, 44910, 51140, 57330, 63670, 60760, 64770, 66240],
+         backgroundColor: 'gray',
+         borderColor: '#293133',
+         borderWidth: 1,
+      }]
+   },
+   options: {
+      //Управление легендой диаграммы
+      legend: {
+         display: true,
+         position: 'bottom'
+      },
+      scales: {
+
+         yAxes: [{
+            ticks: {
+               beginAtZero: false,
+               min: 30000,
+               stepSize: 10000,
+            },
+            gridLines: {
+               color: "black",
+               borderDash: [2, 5],
+            },
+         }],
+
+         xAxes: [{
+            gridLines: {
+               display: false,
+            },
+         }]
+      }
+   }
+});
+
+// Земли промназначения Иркутск
+var ctx = document.getElementById('earth2');
+var earth2 = new Chart(ctx, {
+   type: 'bar',
+   data: {
+      labels: ['11.18', '12.18', '01.19', '02.19', '03.19', '04.19', '05.19', '06.19', '07.19', '08.19', '09.19', '10.19'],
+      datasets: [{
+         label: 'Стоимость кв.м.',
+         data: [66880, 94010, 124380, 95690, 83520, 81900, 99070, 69420, 57780, 62850, 68930, 86200],
+         backgroundColor: '#ab7d5c',
+         borderColor: '#293133',
+         borderWidth: 1,
+      }]
+   },
+   options: {
+      //Управление легендой диаграммы
+      legend: {
+         display: true,
+         position: 'bottom'
+      },
+      scales: {
+
+         yAxes: [{
+            ticks: {
+               beginAtZero: false,
+               min: 00000,
+               stepSize: 50000,
+            },
+            gridLines: {
+               color: "black",
+               borderDash: [2, 5],
+            },
+         }],
+
+         xAxes: [{
+            gridLines: {
+               display: false,
+            },
+         }]
+      }
+   }
+});
+
+// Земли сельхозназначения Иркутск
+var ctx = document.getElementById('earth3');
+var earth3 = new Chart(ctx, {
+   type: 'bar',
+   data: {
+      labels: ['11.18', '12.18', '01.19', '02.19', '03.19', '04.19', '05.19', '06.19', '07.19', '08.19', '09.19', '10.19'],
+      datasets: [{
+         label: 'Стоимость кв.м.',
+         data: [27550, 24640, 26510, 21890, 25170, 29470, 30600, 28420, 32090, 30600, 33280, 33600],
+         backgroundColor: '#BAAF96',
+         borderColor: '#293133',
+         borderWidth: 1,
+      }]
+   },
+   options: {
+      //Управление легендой диаграммы
+      legend: {
+         display: true,
+         position: 'bottom'
+      },
+      scales: {
+
+         yAxes: [{
+            ticks: {
+               beginAtZero: false,
+               min: 10000,
+               stepSize: 10000,
+            },
+            gridLines: {
+               color: "black",
+               borderDash: [2, 5],
+            },
+         }],
+
+         xAxes: [{
+            gridLines: {
+               display: false,
+            },
+         }]
+      }
+   }
+});
+
+// Дома блоки Иркутск
+var ctx = document.getElementById('house');
+var house = new Chart(ctx, {
+   type: 'bar',
+   data: {
+      labels: ['11.18', '12.18', '01.19', '02.19', '03.19', '04.19', '05.19', '06.19', '07.19', '08.19', '09.19', '10.19'],
+      datasets: [{
+         label: 'Стоимость кв.м.',
+         data: [29390, 28650, 30440, 30780, 29470, 30660, 31060, 31140, 32840, 32010, 32100, 31520],
+         backgroundColor: 'gray',
+         borderColor: '#293133',
+         borderWidth: 1,
+      }]
+   },
+   options: {
+      //Управление легендой диаграммы
+      legend: {
+         display: true,
+         position: 'bottom'
+      },
+      scales: {
+
+         yAxes: [{
+            ticks: {
+               beginAtZero: false,
+               min: 20000,
+               stepSize: 5000,
+            },
+            gridLines: {
+               color: "black",
+               borderDash: [2, 5],
+            },
+         }],
+
+         xAxes: [{
+            gridLines: {
+               display: false,
+            },
+         }]
+      }
+   }
+});
+
+// Дома брус Иркутск
+var ctx = document.getElementById('house2');
+var house2 = new Chart(ctx, {
+   type: 'bar',
+   data: {
+      labels: ['11.18', '12.18', '01.19', '02.19', '03.19', '04.19', '05.19', '06.19', '07.19', '08.19', '09.19', '10.19'],
+      datasets: [{
+         label: 'Стоимость кв.м.',
+         data: [26650, 27360, 25290, 24700, 26420, 26950, 27120, 27980, 27600, 28250, 29180, 27380],
+         backgroundColor: '#ab7d5c',
+         borderColor: '#293133',
+         borderWidth: 1,
+      }]
+   },
+   options: {
+      //Управление легендой диаграммы
+      legend: {
+         display: true,
+         position: 'bottom'
+      },
+      scales: {
+
+         yAxes: [{
+            ticks: {
+               beginAtZero: false,
+               min: 20000,
+               stepSize: 5000,
+            },
+            gridLines: {
+               color: "black",
+               borderDash: [2, 5],
+            },
+         }],
+
+         xAxes: [{
+            gridLines: {
+               display: false,
+            },
+         }]
+      }
+   }
+});
+
+// Дома кирпич Иркутск
+var ctx = document.getElementById('house3');
+var house3 = new Chart(ctx, {
+   type: 'bar',
+   data: {
+      labels: ['11.18', '12.18', '01.19', '02.19', '03.19', '04.19', '05.19', '06.19', '07.19', '08.19', '09.19', '10.19'],
+      datasets: [{
+         label: 'Стоимость кв.м.',
+         data: [33980, 38230, 37400, 32180, 30610, 38970, 38020, 37370, 36800, 36900, 37660, 35740],
+         backgroundColor: '#BAAF96',
+         borderColor: '#293133',
+         borderWidth: 1,
+      }]
+   },
+   options: {
+      //Управление легендой диаграммы
+      legend: {
+         display: true,
+         position: 'bottom'
+      },
+      scales: {
+
+         yAxes: [{
+            ticks: {
+               beginAtZero: false,
+               min: 25000,
+               stepSize: 5000,
+            },
+            gridLines: {
+               color: "black",
+               borderDash: [2, 5],
+            },
+         }],
+
+         xAxes: [{
+            gridLines: {
+               display: false,
+            },
+         }]
+      }
+   }
+});
